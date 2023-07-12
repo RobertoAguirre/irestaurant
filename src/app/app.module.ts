a30900747ec4253c1df3388afae799ee49d913dc
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2023-07-12 00:34:18 -0600

    barcode switched to cordova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ReactiveFormsModule,FormsModule,HttpClientModule,IonicStorageModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [BarcodeScanner,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/order-selection/order-selection.page.ts
+++ src/app/order-selection/order-selection.page.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { QrScannerService } from '../services/qr-scanner.service';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 @Component({
   selector: 'app-order-selection',
   templateUrl: './order-selection.page.html',
   styleUrls: ['./order-selection.page.scss'],
 })
-export class OrderSelectionPage {
+export class OrderSelectionPage implements OnInit {
 
-  constructor(private qrScannerService: QrScannerService) { }
+  barcodedata: any;
+  scannResult;
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
-  scanQrCode() {
-    this.qrScannerService.scan();
+  ngOnInit() {
+  }
+
+//once the code is scanned, the data obtained looks like this, 
+// cancelled:false
+// format:"QR_CODE"
+// text:"derp"  <==this is what we want
+
+
+  scan() {
+    this.barcodedata = null;
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.barcodedata = barcodeData;
+      this.scannResult = barcodeData.text;
+    }).catch(err => {
+      console.log('Error', err);
+    });
   }
 
 }
